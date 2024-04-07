2aa75cbd88afd1cf7cda436e2a9264d689f6ea4b
Kartik <kartik_a@mt.iitr.ac.in>
2024-04-07 11:53:53 +0530

    changes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,26 @@
 "use client"
 import ECommerce from "@/components/Dashboard/E-commerce";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import Loader from "@/components/common/Loader";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
-  return (
-    <>
+  const [loading, setloading] = useState(true);
+  useEffect(() => {
+    setloading(false);
+  }, [])
+  if (loading) {
+    <Loader />
+  }
+  else {
+    return (
       <DefaultLayout>
-        <h1>hello</h1>
-        {/* <ECommerce /> */}
+        {/* <h1>hello</h1> */}
+        <ECommerce />
       </DefaultLayout>
-    </>
-  );
+    )
+
+
+  }
 }
