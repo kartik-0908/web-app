208ddeb007bb1c9e2d89a1873ec6d7fa79bb0876
Kartik <kartik_a@mt.iitr.ac.in>
2024-04-07 11:44:28 +0530

    changes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,7 +7,8 @@ export default function Home() {
   return (
     <>
       <DefaultLayout>
-        <ECommerce />
+        <h1>hello</h1>
+        {/* <ECommerce /> */}
       </DefaultLayout>
     </>
   );
