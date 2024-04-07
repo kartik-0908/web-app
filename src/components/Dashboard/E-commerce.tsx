a6eea66414d98389d8809c6d6635b9bf096cdc50
Kartik <kartik_a@mt.iitr.ac.in>
2024-04-07 11:56:34 +0530

    changes

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -1,21 +1,32 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChartOne from "../Charts/ChartOne";
 import ChartTwo from "../Charts/ChartTwo";
 import ChatCard from "../Chat/ChatCard";
+import Loader from "../common/Loader";
 
 const ECommerce: React.FC = () => {
-  return (
-    <>
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
-        <div className="col-span-12">
-          <ChatCard />
+  const [loading, setloading] = useState(true);
+  useEffect(() => {
+    setloading(false);
+  }, [])
+  if (loading) {
+    return <Loader />
+  }
+  else {
+    return (
+      <>
+        <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+          <ChartOne />
+          <ChartTwo />
+          <div className="col-span-12">
+            <ChatCard />
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
+
 };
 
 export default ECommerce;
