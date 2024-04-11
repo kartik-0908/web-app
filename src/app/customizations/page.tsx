1c7cdc70f128398f444dc9c42f0824f1e5f73d54
Kartik <kartik_a@mt.iitr.ac.in>
2024-04-11 22:29:34 +0530

    changes

--- src/app/customizations/page.tsx
+++ src/app/customizations/page.tsx
@@ -662,7 +662,7 @@ const TablesPage = () => {
                   variant="bordered"
                   labelPlacement="outside"
                   placeholder="Enter your Message"
-                  className={`max-w-full h-[60px] pl-2 pr-2 text-${fontColor} disabled:text-white`}
+                  className={`border-red max-w-full h-[60px] pl-2 pr-2 text-${fontColor} disabled:text-white`}
                   endContent={
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
