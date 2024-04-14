7a125618929032a29b7158ad70efd10921ecd6ae
Kartik <kartik_a@mt.iitr.ac.in>
2024-04-14 13:11:50 +0530

    changes

--- src/app/integration/page.tsx
+++ src/app/integration/page.tsx
@@ -2,18 +2,20 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useRouter } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 
 export default async function Integration() {
+  const searchParams = useSearchParams()
   const router = useRouter();
 
   useEffect(() => {
 
     const getShopifyToken = async () => {
-      const urlSearchParams = new URLSearchParams(window.location.search);
-      const code = urlSearchParams.get('code'); // Extract code
-      const shop = urlSearchParams.get('shop'); // Extract code
+      const code = searchParams.get('code'); // Extract code
+      const shop = searchParams.get('shop'); // Extract code
       if (code && shop) {
         try {
+          console.log("insdie useeffect of integration")
           const response = await axios.post('/api/shopify/access-token', { shop, code });
           router.push('/installation')
         } catch (error) {
